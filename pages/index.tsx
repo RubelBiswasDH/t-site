c4df5206103bff8b8f95665b378513e63bebcc1c
RubelBiswasDH <RubelBiswasDH@gmail.com>
2023-09-09 23:57:54 +0600

    Added image draging feature

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,107 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { useEffect, useState, useRef } from 'react'
+import Moveable from "react-moveable";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [target, setTarget]: any = useState(null)
+  const [inputFile, setInputFile]: any = useState(null)
+  const [targetImage, setTargetImage]: any = useState(null)
+
+  const moveableRef: any = useRef(null)
+
+  const onInputChange = (e: any) => {
+    const file: any = e?.target?.files[0]
+    if (file) {
+      setTargetImage(URL.createObjectURL(file))
+    }
+    // console.log(e?.target.files)
+  }
+
+  const onEditButtonClick = () => {
+    setTarget(document.querySelector(".target"))
+  }
+
+  const onSaveButtonClick = () => {
+    // setTargetClass('')
+    setTarget(null)
+    console.log('target')
+  }
+  useEffect(() => {
+    setTarget(document.querySelector(".target"))
+  }, [])
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <Moveable
+        ref={moveableRef}
+        target={target}
+        container={null}
+        origin={true}
+
+        /* Resize event edges */
+        edge={false}
+
+        /* draggable */
+        draggable={true}
+        throttleDrag={0}
+        onDragStart={({ target, clientX, clientY }) => {
+            console.log("onDragStart", target);
+        }}
+        onDrag={({
+          target,
+          beforeDelta, beforeDist,
+          left, top,
+          right, bottom,
+          delta, dist,
+          transform,
+          clientX, clientY,
+        }: OnDrag) => {
+          console.log("onDrag left, top", left, top);
+          // target!.style.left = `${left}px`;
+          // target!.style.top = `${top}px`;
+          console.log("onDrag translate", dist);
+          target!.style.transform = transform;
+        }}
+        onDragEnd={({ target, isDrag, clientX, clientY }) => {
+          console.log("onDragEnd", target, isDrag);
+        }}
+        /* When resize or scale, keeps a ratio of the width, height. */
+        keepRatio={true}
+        resizable={true}
+          throttleResize={0}
+          onResizeStart={({ target , clientX, clientY}) => {
+            console.log("onResizeStart", target);
+          }}
+          onResize={({
+            target, width, height,
+            dist, delta, direction,
+            clientX, clientY,
+          }: any) => {
+            console.log("onResize", target);
+            delta[0] && (target!.style.width = `${width}px`);
+            delta[1] && (target!.style.height = `${height}px`);
+          }}
+          onResizeEnd={({ target, isDrag, clientX, clientY }) => {
+            console.log("onResizeEnd", target, isDrag);
+          }}
+      />
+      <div>
+        <img src="/images/t1.webp" />
+      </div>
+      <div className="container">
+        <div className="target" style={{ width: 300 }}>
+          <img style={{ width: '100%', height: '100%' }} src={targetImage} />
         </div>
       </div>
+      <input onChange={onInputChange} type='file' />
+      <button onClick={onEditButtonClick}>Edit</button>
+      <button onClick={onSaveButtonClick}>Save</button>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   )
 }
